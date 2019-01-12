79b224374a7e54f16c4732361a2050f8de62961f
Subhajit Sahu <wolfram77@gmail.com>
2019-01-12 15:28:08 +0530

    :mushroom: ready to teast

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
+const getStdin = require('get-stdin');
+const boolean = require('boolean');
+const cp = require('child_process');
+const path = require('path');
+const fs = require('fs');
 var amazontts = null;
 var googletts = null;
 
 
 // Global variables
 const E = process.env;
+const STDIO = [0, 1, 2];
 const OPTIONS = {
+  log: boolean(E['TTS_LOG']||'0'),
+  output: E['TTS_OUTPUT']||'out.mp3',
+  text: E['TTS_TEXT']||null,
   provider: E['TTS_PROVIDER']||'google'
 };
 
+
+function name(pro) {
+  if(/amazon|aws/i.test(pro)) return 'extra-amazontts';
+  else return 'extra-googletts';
+};
 
 function load(pro) {
   if(/amazon|aws/i.test(pro)) return amazontts=amazontts||require('extra-amazontts');
@@ -39,10 +53,17 @@ module.exports = tts;
 
 // Run on shell.
 function shell(a) {
-  var o = {};
+  var o = {argv: getStdin()};
   for(var i=2, I=a.length; i<I; i++)
     options(o, k, a, i);
-  var pro = o.provider||OPTIONS.provider
-  return load(pro).shell(a);
+  var pro = o.provider||OPTIONS.provider;
+  var dir = path.dirname(require.resolve(name(pro)));
+  if(o.help) return cp.execSync('less README.md', {cwd: dir, stdio: STDIO});
+  var out = o.output||OPTIONS.output;
+  var txt = o.text? fs.readFileSync(o.text, 'utf8'):o.argv||'';
+  var toc = await load(pro)(out, txt, o);
+  if(o.log || OPTIONS.log) return;
+  for(var c of toc)
+    if(c.title) console.log(c.time+' '+c.title);
 };
 if(require.main===module) shell(process.argv);
